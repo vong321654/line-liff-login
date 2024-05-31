edbad69165f0faaeb8e8505c1f79ffcedaf4fa68
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-05-31 11:16:37 +0700

    Update_Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,55 @@
 "use client"
 import liff from '@line/liff';
-
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const liffId: string = '2005047404-MgGBJNxX';
-const initLiff = async () => {
-  await liff.init({ liffId: liffId })
-  if (!liff.isLoggedIn()) {
-    liff.login()
-    return false
-  }
-  const profile = await liff.getProfile()
-  console.log('profile', profile)
 
-  const getEmail = liff.getDecodedIDToken()
-  console.log('getEmail', getEmail)
+const initLiff = async () => {
+  await liff.init({ liffId: liffId });
+  if (!liff.isLoggedIn()) {
+    liff.login();
+    return false;
+  }
+  const profile = await liff.getProfile();
+  console.log('profile', profile);
 
   const idToken = liff.getDecodedIDToken();
-    console.log('idToken', idToken);
+  console.log('idToken', idToken);
 
+  // รวม ID Token ไว้ในโปรไฟล์
+  const profileWithToken = { ...profile, idToken: idToken };
 
-  return profile
+  return profileWithToken;
 }
-export default function Home() {
 
-  const [init, setInit] = useState<any>(null)
+const logout = () => {
+  liff.logout();
+  window.location.reload();
+}
+
+export default function Home() {
+  const [init, setInit] = useState<any>(null);
 
   useEffect(() => {
-    console.log('useEffect')
-    initLiff()
-      .then((res: any) => {
-        setInit(res)
-      })
-  }, [])
+    console.log('useEffect');
+    initLiff().then((res: any) => {
+      setInit(res);
+    });
+  }, []);
 
   if (!init) {
-    return <h1> Loading... </h1>
+    return <h1> Loading... </h1>;
   }
 
   return (
     <>
-      <h2>  liffId: {liffId}</h2>
       {JSON.stringify(init)}
-      <h2>USER ID LINE : {init?.userId}</h2>
-      <h2>ID Token : {init?.idToken}</h2>
-      <h2>DisplayName : {init?.displayName}</h2>
-      <h2>StatusMessage : {init?.statusMessage}</h2>
-      <img src={init?.pictureUrl} width={200} height={200} alt="profile" />
+      <h2>USER ID LINE : {init?.userId}</h2>&nbsp;
+      <h2>ID Token : {init?.idToken}</h2>&nbsp;
+      <h2>DisplayName : {init?.displayName}</h2>&nbsp;
+      <h2>StatusMessage : {init?.statusMessage}</h2>&nbsp;
+      <img src={init?.pictureUrl} width={200} height={200} alt="profile" />&nbsp;
+      <button onClick={() => logout()} style={{ width: "50%", height: 30 }}>Logout</button>
     </>
   );
 }
