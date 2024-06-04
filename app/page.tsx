57a9c102c9ced5f55a2e66f2c9b17be1a77214e7
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-04 17:00:01 +0700

    get_ID_Token

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 "use client"
 import liff from '@line/liff';
-
 import { useEffect, useState } from 'react'
-
 const logout = () => {
   liff.logout();
   window.location.reload();
 }
-
 const liffId: string = '2005047404-MgGBJNxX';
 const initLiff = async () => {
   await liff.init({ liffId: liffId })
@@ -17,13 +14,12 @@ const initLiff = async () => {
   }
   const profile = await liff.getProfile()
   console.log('profile', profile)
-
+  const idToken = liff.getDecodedIDToken()
+  console.log('idToken', idToken)
   return profile
 }
 export default function Home() {
-
   const [init, setInit] = useState<any>(null)
-
   useEffect(() => {
     console.log('useEffect')
     initLiff()
@@ -31,17 +27,18 @@ export default function Home() {
         setInit(res)
       })
   }, [])
-
   if (!init) {
-    return <h1 style={{textAlign:'center'}}> Loading... </h1>
+    return <h1> Loading... </h1>
   }
 
   return (
     <>
       <h2>USER ID LINE : {init?.userId}</h2>
+      <h2>ID Token: {init?.idToken}</h2>
       <h2>DisplayName : {init?.displayName}</h2>
       <img src={init?.pictureUrl} width={200} height={200} alt="profile" />
-      <button onClick={() => logout()} style={{ width: "20%", height: "30%",marginTop:'5px',textAlign:'center' }}>Logout</button>
+      <button onClick={() => logout()} style={{ width: "20%", height: "30%" }}>Logout</button>
+      
     </>
   );
 }
