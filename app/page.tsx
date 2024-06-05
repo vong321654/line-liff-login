8eb8405c6580a125bdbc27d19cf3b867d60476fa
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-05 15:55:42 +0700

    เอาแค่ ID Token

--- app/page.tsx
+++ app/page.tsx
@@ -35,10 +35,7 @@ export default function Home() {
 
   return (
     <>
-      <h2>USER ID LINE : {init?.userId}</h2>
       <h2>ID Token: {init?.idToken}</h2>
-      <h2>DisplayName : {init?.displayName}</h2>
-      <img src={init?.pictureUrl} width={200} height={200} alt="profile" />
       <button onClick={() => logout()} style={{ width: "20%", height: "30%" }}>Logout</button>
     </>
   );
