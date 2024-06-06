d1ab6d06cd1eefd788385368b526895703af282a
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-06 13:38:53 +0700

    update_config

--- ืnext.config.js
+++ ืnext.config.js
@@ -1,7 +1,7 @@
 require('next-env')();
-module.exports{
+module.exports = {
     env:{
-        NEXT_PUBLIC_LINE_CLIENT_ID = '2005047404-MgGBJNxX',
-        NEXT_PUBLIC_LINE_CLIENT_SECRET = '4a972c07c9d86107c256d82cefaaacce'
+        NEXT_PUBLIC_LINE_CLIENT_ID : '2005047404-MgGBJNxX',
+        NEXT_PUBLIC_LINE_CLIENT_SECRET : '4a972c07c9d86107c256d82cefaaacce'
     },
 }
