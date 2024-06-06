2fac63aba44a144262a337f9c0059e1e84bd5c74
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-06 14:38:52 +0700

    เมื่อไหร่

--- ืnext.config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-module.exports = {
-    env:{
-        NEXT_PUBLIC_LINE_CLIENT_ID : '2005047404-MgGBJNxX',
-        NEXT_PUBLIC_LINE_CLIENT_SECRET : '4a972c07c9d86107c256d82cefaaacce'
-    },
-}
