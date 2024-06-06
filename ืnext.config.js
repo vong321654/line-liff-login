495857f7ad052939e66787819b7a04f8957b19f7
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-06 13:37:57 +0700

    add_Config_file

--- ืnext.config.js
+++ ืnext.config.js
@@ -1,1 +1,7 @@
 require('next-env')();
+module.exports{
+    env:{
+        NEXT_PUBLIC_LINE_CLIENT_ID = '2005047404-MgGBJNxX',
+        NEXT_PUBLIC_LINE_CLIENT_SECRET = '4a972c07c9d86107c256d82cefaaacce'
+    },
+}
