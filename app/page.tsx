ad775ac108a779196cf7d3f952d45010c27b22d6
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-18 17:19:14 +0700

    ทดสอบAPI

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ import Box from '@mui/material/Box';
 import { UserDataModel } from './Type/UserDataModel';
 
 const liffId = process.env.NEXT_PUBLIC_LINE_LIFF_ID as string;
+console.log("LIFF ID:", liffId);
 
 const LineLogin: React.FC = () => {
   const [profile, setProfile] = useState<UserDataModel | null>(null);
   const [loading, setLoading] = useState(true);
-
+  const [message, setMessage] = useState<string | null>(null);
   const initLiff = async () => {
     try {
       await liff.init({ liffId: liffId });
@@ -21,6 +22,11 @@ const LineLogin: React.FC = () => {
       }
       const idToken = liff.getIDToken();
       console.log(idToken);
+      if (idToken) {
+        await sendProfileToBackend(idToken); // ส่ง idToken ถ้ามีค่า
+      } else {
+        console.error('ID Token is null');
+      }
       setLoading(false);
     } catch (error) {
       console.error('LIFF initialization failed', error);
@@ -28,6 +34,24 @@ const LineLogin: React.FC = () => {
     }
   };
 
+  const sendProfileToBackend = async (idToken: string) => {
+    try {
+      const response = await fetch('http://127.0.0.1:5555/v1/profile', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ idToken }), // ส่ง idToken เป็น JSON
+      });
+      if (!response.ok) {
+        throw new Error('Failed to send profile to backend');
+      }
+      setMessage('Profile sent to backend successfully');
+    } catch (error) {
+      console.error('Error sending profile to backend:', error);
+      setMessage('Error sending profile to backend');
+    }
+  };
   useEffect(() => {
     initLiff();
   }, []);
@@ -43,6 +67,7 @@ const LineLogin: React.FC = () => {
   return (
     <Box textAlign="center" mt={5}>
       <h2>ID Token: {profile?.idToken}</h2>
+      <h2>safkajfsafslakn</h2>
     </Box>
   );
 };
