b3d3f8d3c8a2f648bd8f25526537abab5f3f0fe5
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-06-19 13:56:36 +0700

    ทดสอบใหม่

--- app/page.tsx
+++ app/page.tsx
@@ -4,44 +4,52 @@ import { useEffect, useState } from 'react';
 import liff from '@line/liff';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import { UserDataModel } from './Type/UserDataModel';
+import { UserDataModel } from '../sinister/core/Type/UserDataModel';
+import { Button } from '@mui/material';
 
 const liffId = process.env.NEXT_PUBLIC_LINE_LIFF_ID as string;
-console.log("LIFF ID:", liffId);
+console.log('LIFF ID:', liffId);
 
 const LineLogin: React.FC = () => {
   const [profile, setProfile] = useState<UserDataModel | null>(null);
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState<string | null>(null);
-  const initLiff = async () => {
-    try {
-      await liff.init({ liffId: liffId });
-      if (!liff.isLoggedIn()) {
-        liff.login();
-        return;
+  useEffect(() => {
+    const initLiff = async () => {
+      try {
+        await liff.init({ liffId: liffId });
+        if (!liff.isLoggedIn()) {
+          liff.login();
+          return;
+        }
+        const idToken = liff.getIDToken();
+        console.log(idToken);
+        if (idToken) {
+          await sendProfileToBackend(idToken); // ส่ง idToken ถ้ามีค่า
+        } else {
+          console.error('ID Token is null');
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error('LIFF initialization failed', error);
+        setLoading(false);
       }
-      const idToken = liff.getIDToken();
-      console.log(idToken);
-      if (idToken) {
-        await sendProfileToBackend(idToken); // ส่ง idToken ถ้ามีค่า
-      } else {
-        console.error('ID Token is null');
-      }
-      setLoading(false);
-    } catch (error) {
-      console.error('LIFF initialization failed', error);
-      setLoading(false);
-    }
-  };
+    };
+
+    initLiff();
+  }, []);
+
+
 
   const sendProfileToBackend = async (idToken: string) => {
     try {
-      const response = await fetch('https://larry-api.larry-cctv.com/ma-bkk/sit/v1/profile', {
+      const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+      const response = await fetch(`${baseURL}/profile`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ idToken }), // ส่ง idToken เป็น JSON
+        body: JSON.stringify({ idToken }) // ส่ง idToken เป็น JSON
       });
       if (!response.ok) {
         throw new Error('Failed to send profile to backend');
@@ -52,9 +60,6 @@ const LineLogin: React.FC = () => {
       setMessage('Error sending profile to backend');
     }
   };
-  useEffect(() => {
-    initLiff();
-  }, []);
 
   if (loading) {
     return (
@@ -67,7 +72,6 @@ const LineLogin: React.FC = () => {
   return (
     <Box textAlign="center" mt={5}>
       <h2>ID Token: {profile?.idToken}</h2>
-      <h2>safkajfsafslakn</h2>
     </Box>
   );
 };
